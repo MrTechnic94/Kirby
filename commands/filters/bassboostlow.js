80315cb74b43ebe165192fad729d585c858ad66a
MrTechnic94 <bezimienny2@protonmail.com>
2024-09-29 16:44:07 +0200

    Minor update

--- commands/filters/bassboostlow.js
+++ commands/filters/bassboostlow.js
@@ -20,7 +20,7 @@ module.exports = {
         if (queue.filters.ffmpeg.getFiltersEnabled().length >= otherPlayerOptions.maxFiltersEnabled && queue.filters.ffmpeg.isDisabled('bassboost_low')) return message.channel.send({ embeds: [errorEmbeds.max_filters_enabled_error] });
 
         const mode = queue.filters.ffmpeg.isEnabled('bassboost_low') ? 'disabled' : 'enabled';
-        
+
         await queue.filters.ffmpeg.toggle(['bassboost_low', 'normalizer']);
         message.channel.send({ embeds: [createEmbed({ description: `${emoji.checkmark} **Low Bassboost has been \`${mode}\`**` })] });
     },
